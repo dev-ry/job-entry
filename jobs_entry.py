ed6d9bb419a49a5661166c8855f2e58966e9d860
Devin Ryan <devin.ryan7@gmail.com>
2023-05-13 22:52:14 -0500

    Add files via upload

---
+++ jobs_entry.py
@@ -0,0 +1,77 @@
+# GUI tutorial here: https://realpython.com/python-gui-tkinter/
+# sqlite python documentation: https://docs.python.org/3/library/sqlite3.html
+
+import sqlite3
+from tkinter import *
+
+
+def main():
+
+    # create window
+    window = Tk()
+    window.title("Jobs Applied To")
+    window.geometry('400x400')
+
+    # function to store values and insert them into the database
+    def handle_CompanyName():
+        name = enterCompany.get()
+        position = enterPosition.get()
+        date = enterDate.get()
+        # insert event handlers
+        # open connection to database
+        con = sqlite3.connect("jobs_applied.db")
+
+        # create cursor
+        cur = con.cursor()
+
+        # create table if it does not exist
+        cur.execute("create table IF NOT EXISTS jobs_applied(company, position, date)")
+
+        # Insert values saved into database (date as YYYY-MM-DD)
+        cur.execute("insert into jobs_applied (company, position, date) values (?, ?, ?)", (name, position, date))
+
+        # commit
+        con.commit()
+        print("Commit complete")
+
+        # execute query
+        cur.execute("select * from jobs_applied")
+        rows = cur.fetchall()
+
+        # loop the cursor to view the fetchall output
+        for row in rows:
+            print(row)
+
+        # close connection
+        con.close()
+        print(f"On {date}, applied for {position} at {name}")
+
+    # create widgets - 3 labels, 3 entry widgets, and button widget
+    company_label = Label(text="Enter Company Name", width=30)
+    company_label.pack()
+    enterCompany = Entry(window, width=30)
+    enterCompany.pack()
+
+    position_label = Label(text="Enter Position Applied For")
+    position_label.pack()
+    enterPosition = Entry(window, width=30)
+    enterPosition.pack()
+
+    date_label = Label(text="Enter Date Applied on YYYY-MM-DD")
+    date_label.pack()
+    enterDate = Entry(width=30)
+    enterDate.pack()
+
+    Button(window,
+           text="Insert Values into the Database",
+           width=30,
+           padx=5,
+           pady=5,
+           command=handle_CompanyName
+           ).pack()
+
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
